a52666836c3f868a460fa26e8d9e021f93614f62
jun <xxxxxxxx>
2020-10-01 16:23:25 +0900

    slide-menu-sakusei index.html script.js tylesheet.css

--- script.js
+++ script.js
@@ -6,4 +6,25 @@ $(function(){
     $('#cl-modal').click(function(){
         $('#modal').hide();
     });
+
+    $('#btn-slide-menu').click(function(){
+        if(window.matchMedia('(min-width: 900px)').matches){
+            ;
+            //処理なし
+        }else if(window.matchMedia('(min-width: 375px)').matches){
+            if($('#btn-slide-menu').hasClass('open')){
+                $('#btn-slide-menu').removeClass('open');
+                $('#id-slide-menu').slideUp();
+                $('.btn-color').css('color','white');
+            }  
+            else{
+                $('#btn-slide-menu').addClass('open'); 
+                $('#id-slide-menu').slideDown();
+                $('.btn-color').css('color','#008489');    
+            }
+        }else{
+            ; 
+            //処理なし
+        }
+    });
 });
