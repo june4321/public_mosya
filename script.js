a66502badb13ea4d1e2937fbc4c7cddd49483589
jun <xxxxxxxx>
2020-10-02 18:11:01 +0900

    icon-syuusei index.html script.js stylesheet.css

--- script.js
+++ script.js
@@ -7,11 +7,33 @@ $(function(){
         $('#modal').hide();
     });
 
-    $('#op-slide-menu').click(function(){
-        $('#id-slide-menu').slideDown();
+   /* $('#op-slide-menu').click(function(){
+        $('#slide-menu').slideDown();
     });
 
     $('#cl-slide-menu').click(function(){
-        $('#id-slide-menu').slideUp();
+        $('#slide-menu').slideUp();
+    }); */
+
+    $('#op-slide-menu').click(function(){
+        if($(this).hasClass('open')){
+            $(this).removeClass('open');
+            $(this).removeClass('fa-angle-up');
+            $(this).addClass('fa-angle-down');
+            $('#slide-menu').slideUp();
+            if($(window).width() >= 900){
+                $('.icon-color').css('color','#008b8b');
+            }else{
+                $('.icon-color').css('color','white');
+            }
+        }else{
+            $(this).addClass('open');
+            $(this).removeClass('fa-angle-down');
+            $(this).addClass('fa-angle-up');
+            $('#slide-menu').slideDown();
+            $('.icon-color').css('color','#008b8b');
+            }
+        
     });
+
 });
